32e2d95187b02948d85a5b29fe43e053d7322dde
Deviyakaithwas <kaithwasdeviya@gmail.com>
2023-06-02 18:36:29 +0530

    e-commerceAPI

---
+++ config/mongoose.js
@@ -0,0 +1,17 @@
+
+const mongoose = require('mongoose');
+
+// connecting mongoose to its default server and ecommerceDB
+mongoose.connect('mongodb+srv://kaithwasdeviya:p%2540ssw0rd%2527@cluster0.7rszumf.mongodb.net/?retryWrites=true&w=majority', {
+    useNewUrlParser: true
+});
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, "Error connecting to MongoDB"));
+ 
+db.once('open', function(){
+    console.log('Connected to Database :: MongoDB');
+});
+
+module.exports = db; 
